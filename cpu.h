049f5ccb68057d009bee50ecd3521942861a3b54
nadirs <nadirsampaoli@gmail.com>
2012-07-04 17:51:20 +0200

    Fixed cpu flags' bit masks. Added some notes about RAM registers.

--- cpu.h
+++ cpu.h
@@ -3,6 +3,50 @@
  * See the LICENSE for more information
  */
 
+/* --------------------------------------------------------
+ * SPECS:
+ * basic clock frequency:
+ *      f = 4.194394 MHz
+ *
+ * == REGISTERS ==
+ *
+ *  P1      [FF00]
+ *      The P1 ports are connected with a matrix for reading key operations.
+ *  DIV     [FF04]
+ *      The upper 8 bits of the 16-bit counter that counts the basic clock 
+ *      frequency (f) can be referenced. If an LD instruction is executed, 
+ *      these bits are cleared to 0 regardless of the value being written.
+ *
+ * Timer Registers:
+ *  TIMA    [FF05]
+ *      Timer Counter. The main timer unit. Generates an interrupt when it 
+ *      overflows.
+ *  TMA     [FF06]
+ *      Timer Modulo. The value of TMA is loaded when TIMA overflows.
+ *  TAC     [FF07]
+ *      Timer Controller.
+ *
+ * Interrupt Flags:
+ *          bit | priority | vector | name
+ *           0        1      0x0040   V-Blank
+ *           1        2      0x0048   LCDC (STAT referenced)
+ *           2        3      0x0050   Timer overflow
+ *           3        4      0x0058   Serial I/O transfer completion
+ *           4        5      0x0060   P10-P13 terminal negative edge
+ *  IF [FF0F]
+ *      Interrupt Request.
+ *  IE [FFFF]
+ *      Interrupt Enable.
+ *          0: Disabled
+ *          1: Enable
+ *  IME
+ *      Interupt Master Enable.
+ *      0: Reset by DI instruction; prohibits all interrupts.
+ *      1: Set by EI instruction; the interrupt set by IE registers are 
+ *          enabled.
+ * --------------------------------------------------------
+ */
+
 #ifndef CPU_H
 #define CPU_H
 
@@ -10,11 +54,10 @@
 #include <stdint.h>
 
 /* flags (register F) */
-// TODO: bit masks for these flags?
-#define ZF  0x00    // zero flag
-#define CF  0x00    // carry flag
-#define HF  0x00    // half carry flag (nybble verflow)
-#define NF  0x00    // XXX: what's this flag?
+#define ZF  0x80    // zero flag
+#define NF  0x40    // XXX: what's this flag?
+#define HF  0x20    // half carry flag (nybble verflow)
+#define CF  0x10    // carry flag
 
 typedef struct {
     uint8_t lo;
