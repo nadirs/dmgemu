97854291d92b509e733b7896faf306a3797d772a
nadirs <nadirsampaoli@gmail.com>
2012-07-18 00:15:01 +0200

    Add centralized header ("dmgemu.h")

---
+++ dmgemu.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2012, Nadir Sampaoli
+ * See the LICENSE for more information
+ */
+
+#ifndef DMGEMU_H
+#define DMGEMU_H
+
+#include "asm.h"
+#include "cpu.h"
+#include "memory.h"
+
+#endif
