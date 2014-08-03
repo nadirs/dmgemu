556a32d3bf1a428ab5480af8b243e7cc1ae71744
nadirs <nadirsampaoli@gmail.com>
2014-08-03 19:58:16 +0200

    Edit testing sandbox

--- tests/asm_test.c
+++ tests/asm_test.c
@@ -3,17 +3,35 @@
  * See the LICENSE for more information
  */
 
-#include "../src/cpu.h"
-#include "../src/memory.h"
+#include <stdlib.h>
 #include <stdio.h>
-#include <check.h>
+#include <assert.h>
+#include "../src/cpu.h"
+#include "../src/asm.h"
+#include "../src/memory.h"
 
 int main(void)
 {
     cpuinit();
+
     printf("af: 0x%04X (%p)\n", *af, af);
     printf(" a:   0x%02X (%p)\n", *a, a);
     printf(" f:   0x%02X (%p)\n", *f, f);
+
+    uint8_t code[] = {
+        0x21, 0x34, 0x12,   // ld hl, 0x1234
+        0xCB, 0x04,         // rl h
+    };
+    *pc = 0;
+    setmemory(code);
+
+    assert(0x21 == getmembyte(0));
+    // normal code
+    run_opcode();
+    assert(*hl == 0x1234);
+    // CB-prefixed code
+    run_opcode();
+    assert(*h == 0x12 << 1);
 
     return 0;
 }
